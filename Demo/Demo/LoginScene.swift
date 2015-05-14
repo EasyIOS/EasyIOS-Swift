e373fcb3a41202fec57115941598c07ccf1e968f
zhuchao <zhuchao@iosx.me>
2015-05-14 09:56:26 +0800

    push

--- Demo/Demo/LoginScene.swift
+++ Demo/Demo/LoginScene.swift
@@ -13,12 +13,17 @@ class LoginScene: EUScene {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.showBarButton(.LEFT, title: "返回", fontColor: UIColor.greenColor())
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func leftButtonTouch() {
+        URLNavigation.dismissCurrentAnimated(true)
     }
 
     func login (){
